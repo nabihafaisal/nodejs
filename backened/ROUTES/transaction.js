60c90bcc250ab50d779c2abd7efcd80103d7247d
Nabiha <nabihfaisal@gmail.com>
2024-06-23 01:58:00 +0500

    postman testing

--- backened/ROUTES/transaction.js
+++ backened/ROUTES/transaction.js
@@ -1,10 +1,10 @@
-const {addIncome} = require ('../controllers/income');
+const {addIncome, getIncome, deleteIncome} = require ('../CONTROLLERS/income');
 const router= require('express').Router()
 
 
 router.post('/add-income' , addIncome)
+      .get('/get-income',getIncome)
+      .delete('/delete-income/:id', deleteIncome )
 
 module.exports=router
 
-
-module.exports= router
--- backened/controllers/income.js
+++ backened/controllers/income.js
@@ -1,29 +1,53 @@
-const IncomeSchema =require("../models/incomemodel")
+const IncomeSchema = require("../MODELS/incomemodel")
 
-exports.addIncome= async (req, res) => {
-   const {title, amount, category, description, date}  = req.body
+exports.addIncome = async (req, res) => {
+    const { title, amount, category, description, date } = req.body
 
-   const income=IncomeSchema({
-    title,
-    amount,
-    category,
-    description,
-    date
-   })
-   try{
-    if(!title || !category || !description || !date){
-        return res.status(400).json({message:"Please fill all the fields" })
+    const income = IncomeSchema({
+        title,
+        amount,
+        category,
+        description,
+        date
+    })
+    try {
+        if (!title || !category || !description || !date) {
+            return res.status(400).json({ message: "Please fill all the fields" })
+        }
+        if (amount <= 0 || !amount === 'number') {
+            return res.status(400).json({ message: "Invalid amount" })
+        }
+        await income.save()
+        res.status(200).json({ message: "Income Added" })
     }
-    if(amount <=0 || !amount ==='number'){
-        return res.status(400).json({message:"Invalid amount" })
-    }
-    await income.save()
-    res.status(200).json({message :"Income Added"})
-   }
-   catch(error){
-   
-   }
+    catch (error) {
+         res.status(500).json({message :"server error"})
 
-   console.log(income)
+    }
+
+    console.log(income)
+
+}
+exports.getIncome=async(req,resp)=>{
+    try {
+        const income=await IncomeSchema.find().sort({createAt: -1})
+        resp.status(200).json(income)
+    } catch (error) {
+        resp.status(500).json({message:'server error'})
+    }
+
+}
+
+exports.deleteIncome=async(req,resp)=>{
+    const{id}=req.params;
+    console.log(params)
+    IncomeSchema.findByIdAndDelete(id)
+        .then((income)=>{
+        resp.status(200).json({message:'income deleted'})
+
+        .catch((err)=>{
+            resp.status(500).json({message:'server error'}) 
+        })
+    })
 
 }
