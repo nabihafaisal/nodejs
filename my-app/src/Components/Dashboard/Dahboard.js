a7aa2097945d15879c0b4c1b8361d7a8f2af4774
KhadeejaArshadAli <Khadijaarshad435@gmail.com>
2024-06-25 17:52:48 +0500

    chartdisplay

--- my-app/src/Components/Dashboard/Dahboard.js
+++ my-app/src/Components/Dashboard/Dahboard.js
@@ -1,23 +1,32 @@
-import React from "react";
-import styled from "styled-components";
-import { InnerLayout } from "../../styles/Layoutes";
+import React from 'react'
+import styled from 'styled-components'
+import { InnerLayout } from '../../styles/Layoutes';
+import Chart from '../Chart/chart';
 
 function Dashboard() {
+   
+
+
     return (
         <DashboardStyled>
-         <InnerLayout> 
-            <h1>All Transactions</h1>
-            <div className="stats-con">
-                <div className="chart-con"></div>
-            </div>
-
-        
-         </InnerLayout>
+            <InnerLayout>
+                <h1>All Transactions</h1>
+                <div className="stats-con">
+                    <div className="chart-con">
+                        <Chart />
+                       
+                    </div>
+                
+                </div>
+            </InnerLayout>
         </DashboardStyled>
     )
 }
 
 const DashboardStyled = styled.div`
+   
+
+       
 `;
 
 export default Dashboard
